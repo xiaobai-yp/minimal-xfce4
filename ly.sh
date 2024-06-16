e517a02d31772232c0a6eabb73d6ead98175423c
xiaobai-yp <emperorgod19@gmail.com>
2024-06-16 13:18:53 +0700

    Update ly.sh

--- ly.sh
+++ ly.sh
@@ -5,8 +5,8 @@ sudo apt install -y libpam0g-dev libxcb-xkb-dev
 # Install Ly Console Display Manager
 cd 
 cd Downloads
-git clone --recurse-submodules https://github.com/fairyglade/ly
-cd ly/
+git clone https://github.com/xiaobai-yp/Ly.git
+cd Ly/
 make
 sudo make install installsystemd
 sudo systemctl enable ly.service
